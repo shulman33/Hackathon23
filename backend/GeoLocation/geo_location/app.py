6bcd2323e4452959803e03f94e0d5ff0a0af50d0
shulman33 <samshulman6@gmail.com>
2023-04-30 12:21:12 -0400

    geo location API

--- backend/GeoLocation/geo_location/app.py
+++ backend/GeoLocation/geo_location/app.py
@@ -1,9 +1,18 @@
 import json
 import boto3
 from utils import haversine_distance
+from decimal import Decimal
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super(DecimalEncoder, self).default(obj)
+
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('FinancialServiceProviders')
+table = dynamodb.Table('FinServicesTable')
 
 
 def fetch_service_providers_from_database():
@@ -14,33 +23,38 @@ def fetch_service_providers_from_database():
 def get_service_providers_near_user(user_location, max_distance=10):
     all_service_providers = fetch_service_providers_from_database()
     nearby_service_providers = list(filter(lambda provider: haversine_distance(
-        user_location['latitude'],
-        user_location['longitude'],
-        provider['latitude'],
-        provider['longitude']
+        float(user_location['latitude']),
+        float(user_location['longitude']),
+        float(provider['latitude']),
+        float(provider['longitude'])
     ) <= max_distance, all_service_providers))
 
     sorted_providers = sorted(nearby_service_providers, key=lambda provider: haversine_distance(
-        user_location['latitude'],
-        user_location['longitude'],
-        provider['latitude'],
-        provider['longitude']
+        float(user_location['latitude']),
+        float(user_location['longitude']),
+        float(provider['latitude']),
+        float(provider['longitude'])
     ))
-
     return sorted_providers
 
 
 def lambda_handler(event, context):
-
     user_location = {
-        'latitude': event['latitude'],
-        'longitude': event['longitude']
+        'latitude': float(event['queryStringParameters']['latitude']),
+        'longitude': float(event['queryStringParameters']['longitude'])
     }
-    max_distance = event.get('max_distance', 10)
+    max_distance = float(event.get('queryStringParameters', {}).get('max_distance', 10))
 
     nearby_service_providers = get_service_providers_near_user(user_location, max_distance)
 
     return {
         'statusCode': 200,
-        'body': json.dumps(nearby_service_providers)
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        'body': json.dumps(nearby_service_providers, cls=DecimalEncoder)
     }
+
+
